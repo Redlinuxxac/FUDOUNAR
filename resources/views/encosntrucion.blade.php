4ac99d400877448ffae0c5b7ddd98d5dae1e15b3
Edwin R. Rosario <Rosario_Edwin@hotmail.com>
2024-12-09 16:41:10 -0400

    actualizacion de origen

---
+++ resources/views/encosntrucion.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Fudcounar</title>
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/fudounar.css">
+</head>
+<body>
+    <header id="header">
+        <img src="img/logo.jpg" alt="Logo Fudcounar">
+        <nav>
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Actividades</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div class="contenedor">
+        <div class="div-dominicana"></div>
+        <div class="div-aruba"></div>
+    </div>
+
+    <main>
+        <div>
+             <h1>Bienvenido a Fudcounar</h1>
+             <p>Este es un texto de ejemplo para rellenar el contenido de la página. Aquí puedes colocar información sobre tu organización, sus objetivos y actividades.</p>
+             <img src="https://api.lorem.space/image/face?w=150&amp;amp;amp;amp;h=220" alt="Imagen de ejemplo">
+        </div>
+        <div>
+             <h1>Bienvenido a Fudcounar</h1>
+             <p>Este es un texto de ejemplo para rellenar el contenido de la página. Aquí puedes colocar información sobre tu organización, sus objetivos y actividades.</p>
+             <img src="https://fakeimg.pl/250x100/?text=Evento 2" alt="Imagen de ejemplo">
+        </div>
+    </main>
+
+    <footer>
+        <p>&copy; Fudcounar 2023</p>
+    </footer>
+
+    <script src="assets/js/script.js"></script>
+</body>
+</html>
