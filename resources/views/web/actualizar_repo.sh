2e48abdddc81a5d2081a25f244788e86a5adbcf4
Edwin R. Rosario <Rosario_Edwin@hotmail.com>
2024-12-14 16:31:23 -0400

    actualiacion

--- resources/views/web/actualizar_repo.sh
+++ resources/views/web/actualizar_repo.sh
@@ -3,3 +3,9 @@
 git fetch origin
 # Une los cambios locales con los remotos de la rama master
 git pull origin master
+
+if [ $? -eq 0 ]; then
+  echo "Actualización completada exitosamente."
+else
+  echo "Error al actualizar el repositorio."
+fi
