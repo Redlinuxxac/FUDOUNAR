28d71af9f4a33e92e9dfd4aac5200bd12f256e3a
Edwin R. Rosario <Rosario_Edwin@hotmail.com>
2024-12-14 16:20:36 -0400

    aggragando la pagian de cocantos

--- resources/views/web/actividad.blade.php
+++ resources/views/web/actividad.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('title')
-    Inicio
+    Actividades
 @endsection
 @section('menu')
     @include('menu', ['lugar' => 'Inicio'])
--- resources/views/web/contact.blade.php
+++ resources/views/web/contact.blade.php
@@ -1,16 +1,30 @@
 @extends('layouts.app')
 @section('title')
-    Inicio
+   Contacto
 @endsection
 @section('menu')
     @include('menu', ['lugar' => 'Inicio'])
 @endsection
 @section('content')
-<div class="row">
-    <div class="noti">
-        <h1>Bienvenido a Fudcounar</h1>
-        <img src="https://picsum.photos/200/300" alt="Imagen de ejemplo">
-        <p>Este es un texto de ejemplo para rellenar el contenido de la página. Aquí puedes colocar información sobre tu organización, sus objetivos y actividades.</p>                
-    </div>
-</div>
+<h1>Contáctanos</h1>
+    @if(session('success'))
+        <div class="alert alert-success">{{ session('success') }}</div>
+    @endif
+
+    <form method="POST" action="{{ route('contact.store') }}">
+        @csrf
+        <div>
+            <label for="name">Nombre:</label>
+            <input type="text" name="name" id="name" required>
+        </div>
+        <div>
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email" required>
+        </div>
+        <div>
+            <label for="message">Mensaje:</label>
+            <textarea name="message" id="message" required></textarea>
+        </div>
+        <button type="submit">Enviar</button>
+    </form>
 @endsection
