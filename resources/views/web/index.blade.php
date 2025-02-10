9d55b457c1f34c10aae6597598adf34147f8f6df
Edwin R. Rosario <Rosario_Edwin@hotmail.com>
2025-02-10 17:37:52 -0400

    feat: :bug: complendo el  diseño de index

--- resources/views/web/index.blade.php
+++ resources/views/web/index.blade.php
@@ -36,7 +36,27 @@
             <h2>Actividades Realizadas</h2>
         </div>
     </div>
-    <!-- codigo aqui -->
+    <!-- codigo aqui -->@foreach ($posts as $post)    
+    <div class="row">
+        <div class="col-md-12">
+            <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+              <div class="col p-4 d-flex flex-column position-static">
+                <strong class="d-inline-block text-primary-emphasis">World</strong>
+                <h3 class="mb-0">{{ $post->title }}</h3>
+                <div class="mb-1 text-body-secondary">Nov 12</div>
+                <p class="card-text mb-auto">{!! substr($post->body,0,1000) !!}...</p>
+                <a href="#" class="icon-link gap-1 icon-link-hover stretched-link">
+                  Continue reading
+                  <svg class="bi"><use xlink:href="#chevron-right"/></svg>
+                </a>
+              </div>
+              <div class="col-4 d-none d-sm-block d-lg-block">
+                  <img src="img/{{ $post->imagen }}" class="img-fluid" alt="Imagen de ejemplo">
+              </div>
+            </div>
+          </div>
+    </div>
+@endforeach
 </div>
 @endsection
 @section('footer')
